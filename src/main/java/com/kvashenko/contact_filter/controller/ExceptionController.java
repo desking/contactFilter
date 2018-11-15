c3b5b5e3725b175c85877cd43a2e3a7a0dcf8dca
desking <dimakvashenko@mail.ru>
2018-11-15 16:28:05 +0200

    Update ExceptionController.java

--- src/main/java/com/kvashenko/contact_filter/controller/ExceptionController.java
+++ src/main/java/com/kvashenko/contact_filter/controller/ExceptionController.java
@@ -6,11 +6,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.regex.PatternSyntaxException;
+
 @ControllerAdvice
 public class ExceptionController {
+
+    @ExceptionHandler(value = PatternSyntaxException.class)
+    public ResponseEntity<Error> patternException(PatternSyntaxException exception) {
+        Error error = new Error(HttpStatus.BAD_REQUEST, "Invalid regular expression " + exception.getPattern(), exception);
+        return new ResponseEntity<>(error, error.getStatus());
+    }
+
     @ExceptionHandler(value = Throwable.class)
     public ResponseEntity<Error> exception(Throwable exception) {
-        Error error = new Error(HttpStatus.NOT_FOUND, exception);
+        Error error = new Error(HttpStatus.INTERNAL_SERVER_ERROR, exception);
         return new ResponseEntity<>(error, error.getStatus());
     }
 }
